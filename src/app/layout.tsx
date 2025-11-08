9667b1972a0f188cd121937df924361a5cedece4
altafxx <altafhasnan@gmail.com>
2025-11-08 08:34:23 +0800

    chore: restructure styling files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
+import "@/styles/excalidraw.css";
+import "@/styles/reactflow.css";
 import { Providers } from "@/components/providers";
 
 const geistSans = Geist({
